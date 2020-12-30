1d19ed0b7259b5004189bd186f3821d90fc19b7a
emanoeldelfino <emanoeldelfno@gmail.com>
2020-12-30 00:35:39 -0300

    Update css, jquery and template

--- blog/static/js/jquery-confirm.js
+++ blog/static/js/jquery-confirm.js
@@ -1,3 +1,3 @@
-$(document).on('click', '.delete-post, .comment-approve, .publish-post', function(){
+$(document).on('click', '.confirm', function(){
     return confirm('Are you sure you want to do this?');
 })
